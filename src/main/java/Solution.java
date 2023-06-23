6178c5372475e66338000865f131c999e8d22ee4
alek <wait82985@gmail.com>
2023-06-23 14:34:41 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,17 @@
 public class Solution {
     public int[] divisibilityArray(String word, int m) {
-        //TODO
-        return null;
+        int[] div = new int[word.length()];
+        long remainder = 0;
+        for (int i = 0; i < word.length(); i++) {
+            long curNum = remainder * 10 + (word.charAt(i) - '0');
+            if (curNum % m == 0) {
+                div[i] = 1;
+                remainder = 0;
+            } else {
+                remainder = curNum % m;
+            }
+        }
+
+        return div;
     }
 }
